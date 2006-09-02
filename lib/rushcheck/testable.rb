4c021660e77a53f37f3a080096102b16538915f9
ikegami <ikegami@madscientist.jp>
2006-09-02 20:05:43 +0100

    add a batch procedure to testable.rb

--- lib/rushcheck/testable.rb
+++ lib/rushcheck/testable.rb
@@ -3,6 +3,8 @@
 # See also QuickCheck in Haskell
 
 require 'rushcheck/config'
+require 'rushcheck/random'
+require 'rushcheck/testresult'
 
 module Testable 
 
@@ -29,13 +31,80 @@ module Testable
   end
   alias collect :label
 
+  def run(opts)
+    RushCheckConfig.batch(opts.ntests, opts.debug?).
+      tests_batch(property, StdGen.new(0))
+  end
+
+  def rjustify(n, s)
+    ' ' * [0, n - s.length].max + s
+  end
+  private :rjustify
+
+  def try_test(ts)
+    ntests = 1
+    count  = 0
+    others = []
+    if ! ts.empty?
+      ts.each do |t|
+        begin
+          r = t.call(opts)
+          case r
+          when TestOk
+            puts "."
+            ntests += 1
+            count  += r.ntests
+          when TestExausted
+            puts "?"
+            ntests += 1
+            count += r.ntests
+          when TestFailed
+            puts "#"
+            ntests += 1
+            others << [r.results, ntests, r.ntests]
+            count += r.ntests
+          else
+            raise(RuntimeError, "RushCheck: illegal result")
+          end
+        rescue
+          puts "*"
+          ntests += 1
+          next
+        end
+      end
+    end
+    print(rjustify([0, 35-ntests].max, " (") + count.to_s + ")\n")
+    others
+  end
+  private :try_test
+
+  def final_run(f, n, no, name)
+    puts
+    puts "    ** test " + n.to_s + " of " + name + " failed with the binding(s)"
+    f.each do |v|
+      puts "    **  " + v.to_s
+    end
+    puts
+  end
+  private :final_run
+
+  def run_tests(name, opts, tests)
+    print(rjustify(25, name) + " : ")
+    f = try_test(tests)
+    f.each { |f, n, no| final_run(f, n, no, name) }
+    nil
+  end
+
   def test
     check(RushCheckConfig.verbose)
   end
   alias verbose_check :test
 
+  def testcase
+    Proc.new {|opts| run(opts)}
+  end
+
   def trivial
     classify('trivial') { yield }
   end
-
 end
