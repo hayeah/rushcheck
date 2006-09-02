680e7fe8d11a8f402350e63dde96644ffa519d23
ikegami <ikegami@madscientist.jp>
2006-09-02 20:03:42 +0100

    add testoptions and testresult

---
+++ lib/rushcheck/testresult.rb
@@ -0,0 +1,21 @@
+# = testresult.rb
+# 
+
+class TestResult
+  attr_reader :message, :ntests, :stamps
+  def initialize(message, ntests, stamps)
+    @message, @ntest, @stamps = message, ntests, stamps
+  end
+end
+
+class TestOk < TestResult; end
+
+class TestExausted < TestResult; end
+
+class TestFailed
+  attr_reader :results, :ntests
+  def initialize(results, ntests)
+    @results, @ntests = results, ntests
+  end
+end
+
