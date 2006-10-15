87ab7090c79dcf3cb8343c01da78b3644fab250d
ikegami <ikegami@madscientist.jp>
2006-10-15 05:59:57 +0100

    add PositiveInteger and NegativeInteger in integer.rb

--- lib/rushcheck/integer.rb
+++ lib/rushcheck/integer.rb
@@ -46,3 +46,53 @@ class Integer
   end
 
 end
+
+class PositiveInteger < Integer
+
+  @@max_bound = 2**30 - 1
+  @@min_bound = 1
+
+  # this method is needed to include HsRandom.
+  def self.random_range(gen, lo=@@min_bound, hi=@@max_bound)
+    hi, lo = lo, hi if hi < lo 
+    raise RuntimeError, "PositiveInteger requires positive lower bound." if lo <= 0
+    v, g = gen.gen_next
+    d = hi - lo + 1
+
+    if d == 1
+    then [lo, g]
+    else [(v % d) + lo, g]
+    end
+  end
+
+  # this method is needed to use Arbitrary.
+  def self.arbitrary
+    RushCheck::Gen.sized {|n| RushCheck::Gen.choose(1, n) }
+  end
+
+end
+
+class NegativeInteger < Integer
+
+  @@max_bound = -1
+  @@min_bound = -(2**30)
+
+  # this method is needed to include HsRandom.
+  def self.random_range(gen, lo=@@min_bound, hi=@@max_bound)
+    hi, lo = lo, hi if hi < lo 
+    raise RuntimeError, "NegativeInteger requires negative upper bound." if hi >= 0
+    v, g = gen.gen_next
+    d = hi - lo + 1
+
+    if d == 1
+    then [lo, g]
+    else [(v % d) + lo, g]
+    end
+  end
+
+  # this method is needed to use Arbitrary.
+  def self.arbitrary
+    RushCheck::Gen.sized {|n| RushCheck::Gen.choose(-n, -1) }
+  end
+
+end
