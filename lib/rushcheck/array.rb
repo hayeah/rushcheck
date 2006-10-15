e6db0b145c9c534d002dc88402b358b9b37152fe
ikegami <ikegami@madscientist.jp>
2006-10-15 06:01:07 +0100

    change set_pattern a bit in array.rb
    allow to the following notation (array of arrays)
    MyRandomArray.set_pattern([Integer, String]) {
      |ary, i|
         [Integer, String]
    }
    ===> MyRandomArray.arbitrary gives a Gen of [[Integer, String], [Integer, String], ...]

--- lib/rushcheck/array.rb
+++ lib/rushcheck/array.rb
@@ -16,7 +16,8 @@ class RandomArray < Array
   # self.set_pattern must be executed before calling self.arbitrary.
   # self.set_pattern defines pattern of random arrays for self.arbitrary.
   # self.set_pattern takes a variable and a block, where the variable
-  # is used for the first element of random array. On the other hand,
+  # should be a class which is used for the first element of random array.
+  # On the other hand,
   # the block should define random array by inductive way; the block
   # takes two variables and the first variable is assumed as an array
   # and the second variable is the index of array.
@@ -25,14 +26,25 @@ class RandomArray < Array
     nil
   end
 
+  def self.create_components(cs, n, r)
+    case cs
+    when Class
+      cs.arbitrary.value(n, r)
+    when Array
+      cs.map {|c| self.create_components(c, n, r)}
+    else
+      raise RuntimeError, "Unexpected arguments #{cs.inspect}. Maybe forgotten calling set_pattern before?"
+    end
+  end
+
   # a private method for self.arbitrary
   def self.create_array(len)
     RushCheck::Gen.new do |n, r|
-      ary = [@@base.class.arbitrary.value(n, r)]
+      ary = [self.create_components(@@base, n, r)]
       r2 = r
       (1..len).each do |i|
         r1, r2 = r2.split
-        ary << @@indp.call(ary, i).class.arbitrary.value(n, r1)
+        ary << self.create_components(@@indp.call(ary, i), n, r1)
       end
       
       ary
